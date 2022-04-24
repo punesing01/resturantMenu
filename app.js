c6955ad284c3d09418f094a0db1bccb35ce07d9e
punesing01 <puneeshsingh001@gmail.com>
2022-04-23 21:45:05 -0700

    added menu items with styling

---
+++ app.js
@@ -0,0 +1,324 @@
+const menu = [
+    {
+        id:1,
+        category:'breakfast',
+        description: 'Assorted biscuits',
+        price:12,
+        img: 'imgs/assorted_biscuits.jpg',
+        text:'Make Teatime Exciting With A Fresh, Buttery And Delectable Variety Of Assorted Cookies. From Chocolate, Strawberry-Filled, Almond, Pistachio, Theres Something For Everyone! Baked With Finest Quality Ingredients.'
+    },
+    {
+        id:2,
+        category:'breakfast',
+        description: 'Avocado egg sandwitch',
+        price:18,
+        img: 'imgs/avocado_egg_sandwitch.jpg',
+        text:'Fresh avocado sandwitch with a tiny bit of mayonnaise on top of soft boiled eggs, celery, lemon juice, and herbs'
+    },
+    /*{
+        id:3,
+        category:'breakfast',
+        description: 'Spinach sandwitch',
+        price:15,
+        img: 'imgs/baby_spinach_sandwitch.jpg'
+    },
+    {
+        id:4,
+        category:'breakfast',
+        description: 'Banana oatmeal',
+        price:12,
+        img: 'imgs/banana_oatmeal.jpg'
+    },
+    {
+        id:5,
+        category:'breakfast',
+        description: 'Banana strawberry smoothie',
+        price:10,
+        img: 'imgs/banana_straberry_smoothie.jpg'
+    },
+    {
+        id:6,
+        category:'lunch',
+        description: 'Beef burger with fries',
+        price:23,
+        img: 'imgs/beef_burger.jpg'
+    },*/
+    {
+        id:7,
+        category:'lunch',
+        description: 'Beef sandwitch',
+        price:20,
+        img: 'imgs/beef_sandwitch.jpg',
+        text:'Beef up your dinnertime with Italian Beef Sandwiches. Topped with drained giardiniera and shredded cheese, these Italian Beef Sandwiches are delicious and ready in under a half-an-hour.'
+    },
+    /*{
+        id:8,
+        category:'drinks',
+        description: 'Beet root juice',
+        price:10,
+        img: 'imgs/beet_root_juice.jpg'
+    },
+    {
+        id:9,
+        category:'lunch',
+        description: 'Chicken with brocolli',
+        price:22,
+        img: 'imgs/chicken_brocolli.jpg'
+    },
+    {
+        id:10,
+        category:'breakfast',
+        description: 'Chocolate cake',
+        price:12,
+        img: 'imgs/chocolate_cake.jpg'
+    },
+    {
+        id:11,
+        category:'drinks',
+        description: 'Chocolate Shake',
+        price:17,
+        img: 'imgs/chocolate_shake.jpg'
+    },
+    {
+        id:12,
+        category:'drinks',
+        description: 'Coconut smoothie',
+        price:15,
+        img: 'imgs/coconut_smoothie.jpg'
+    },*/
+    {
+        id:13,
+        category:'lunch',
+        description: 'Cod fish',
+        price:25,
+        img: 'imgs/cod_fish_lunch.jpg',
+        text:'Baked Cajun Garlic Butter Cod is a tender and juicy cod filet baked in a buttery cajun seasoning with garlic cloves and olive oil making this cod a flavorful and delicious lunch'
+    },
+    /*{
+        id:14,
+        category:'breakfast',
+        description: 'Dark chocolate fantasy',
+        price:15,
+        img: 'imgs/dark_chocolate_cake.jpg'
+    },
+    {
+        id:15,
+        category:'breakfast',
+        description: 'Donuts',
+        price:12,
+        img: 'imgs/donuts.jpg'
+    },*/
+    {
+        id:16,
+        category:'lunch',
+        description: 'Egg with rice',
+        price:18,
+        img: 'imgs/egg_and_rice.jpg'
+    },
+    {
+        id:17,
+        category:'lunch',
+        description: 'Egg salad',
+        price:18,
+        img: 'imgs/egg_salad.jpg'
+    },
+    {
+        id:18,
+        category:'breakfast',
+        description: 'Fresh fruits',
+        price:22,
+        img: 'imgs/fruits.jpg'
+    },
+     {
+        id:19,
+        category:'drinks',
+        description: 'Fruity smoothie',
+        price:16,
+        img: 'imgs/fruity_smoothie.jpg'
+    },
+    {
+        id:20,
+        category:'lunch',
+        description: 'Currey with rice',
+        price:22,
+        img: 'imgs/indian_currey.jpg'
+    }, /*{
+        id:21,
+        category:'drinks',
+        description: 'Kale smoothie',
+        price:14,
+        img: 'imgs/kale-smoothie.jpg'
+    }, {
+        id:22,
+        category:'drinks',
+        description: 'Kiwi punch',
+        price:12,
+        img: 'imgs/kiwi_punch.jpg'
+    }, */{
+        id:23,
+        category:'drinks',
+        description: 'latte',
+        price:8,
+        img: 'imgs/latte.jpg'
+    }, /*{
+        id:24,
+        category:'driks',
+        description: 'Lemon mint refresher',
+        price:12,
+        img: 'imgs/lemon_mint_refresher.jpg'
+    },*/ {
+        id:25,
+        category:'breakfst',
+        description: 'Oatmeal with greek yogurt',
+        price:12,
+        img: 'imgs/oatmeal_greek_yogurt.jpg'
+    },
+    /*{
+        id:26,
+        category:'drinks',
+        description: 'Orange apricot juice',
+        price:16,
+        img: 'imgs/orange_apricot_juice.jpg'
+    },
+    {
+        id:27,
+        category:'drinks',
+        description: 'Orange smoothie',
+        price:16,
+        img: 'imgs/orange_smoothie.jpg'
+    },
+    {
+        id:28,
+        category:'breakfast',
+        description: 'Pancakes',
+        price:16,
+        img: 'imgs/pancakes.jpg'
+    },
+    {
+        id:29,
+        category:'lunch',
+        description: 'Paneer tikka rice',
+        price:24,
+        img: 'imgs/paneer_tikka_rice.jpg'
+    }*/,
+    {
+        id:30,
+        category:'lunch',
+        description: 'Pork belly',
+        price:26,
+        img: 'imgs/pork_belly.jpg'
+    },
+    {
+        id:31,
+        category:'lunch',
+        description: 'Potato fries',
+        price:9,
+        img: 'imgs/potato_fries.jpg'
+    },
+    /*{
+        id:32,
+        category:'lunch',
+        description: 'Salad',
+        price:16,
+        img: 'imgs/Salad.jpg'
+    }*/,
+    {
+        id:33,
+        category:'lunch',
+        description: 'Roasted pepper Soup',
+        price:10,
+        img: 'imgs/soup.jpg'
+    },
+    {
+        id:34,
+        category:'lunch',
+        description: 'Speghetti',
+        price:18,
+        img: 'imgs/spegattie.jpg'
+    },
+    {
+        id:35,
+        category:'lunch',
+        description: 'Spinach sandwitch',
+        price:16,
+        img: 'imgs/spinach_sandwitch.jpg'
+    },
+    /*{
+        id:36,
+        category:'lunch',
+        description: 'Strawberry pizza',
+        price:18,
+        img: 'imgs/strawberry_pizza.jpg'
+    }
+    {
+        id:37,
+        category:'lunch',
+        description: 'Sushie',
+        price:22,
+        img: 'imgs/sushie.jpg'
+    },
+    {
+        id:38,
+        category:'lunch',
+        description: 'Veg. pizza',
+        price:18,
+        img: 'imgs/veg_pizza.jpg'
+    },
+    {
+        id:39,
+        category:'lunch',
+        description: 'Pasta in white sauce',
+        price:22,
+        img: 'imgs/white_pasta.jpg'
+    },
+    {
+        id:40,
+        category:'lunch',
+        description: 'Yam fries',
+        price:9,
+        img: 'imgs/yam_fries.jpg'
+    },*/
+];
+
+const menuItems = document.querySelector('.menu-section');
+const menuOptions = document.querySelectorAll('.menu-bar nav li a');
+
+window.addEventListener('DOMContentLoaded',function(){
+     displayMenu(menu);
+});
+
+console.log(menuOptions);
+Array.from(menuOptions).forEach(function(menuBarItem){
+    menuBarItem.addEventListener('click', function(event){
+        console.log(event.target.innerText);
+        console.log('dataid=',event.currentTarget.dataset.id);
+        let seletedMenu = menu.filter(function(item){
+            return (event.target.innerText.toLowerCase() === item.category) ? item : null;
+        });
+        console.log(seletedMenu);
+        if(event.currentTarget.dataset.id === 'all'){
+            displayMenu(menu);
+        } else displayMenu(seletedMenu);
+})});
+
+function displayMenu(menuToDisplay){
+    const newMenu = menuToDisplay.map(function(item){
+        return `
+            <artcile class="item-container">
+                <h3 class="item-header"></h3>
+                <div class="item-image-container">
+                    <img class="item-image" src="${item.img}" alt="${item.description}">
+                </div>
+                <div class="item-detail-container">
+                    <div class="item-text">
+                      <p>${item.description}</p>
+                      <p>$${item.price}</p>
+                    </div>
+                    <div class="item-desc">
+                        <p>${item.text}</p>
+                    </div>
+                </div>
+               </artcile>`;
+    });
+    menuItems.innerHTML = newMenu.join('');
+}
