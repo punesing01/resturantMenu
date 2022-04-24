52389edd742ba2c90a382a125a041a71842e4c0d
punesing01 <puneeshsingh001@gmail.com>
2022-04-23 23:43:50 -0700

    fix styles for respomsive design

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const menu = [
         description: 'Assorted biscuits',
         price:12,
         img: 'imgs/assorted_biscuits.jpg',
-        text:'Make Teatime Exciting With A Fresh, Buttery And Delectable Variety Of Assorted Cookies. From Chocolate, Strawberry-Filled, Almond, Pistachio, Theres Something For Everyone! Baked With Finest Quality Ingredients.'
+        text:'Make Teatime Exciting With A Fresh, Buttery And Variety Of Assorted Cookies. From Chocolate, Strawberry-Filled, Almond, Pistachio, Theres Something For Everyone!'
     },
     {
         id:2,
@@ -15,34 +15,15 @@ const menu = [
         img: 'imgs/avocado_egg_sandwitch.jpg',
         text:'Fresh avocado sandwitch with a tiny bit of mayonnaise on top of soft boiled eggs, celery, lemon juice, and herbs'
     },
-    /*{
+    {
         id:3,
         category:'breakfast',
-        description: 'Spinach sandwitch',
+        description: 'Egg muffin',
         price:15,
-        img: 'imgs/baby_spinach_sandwitch.jpg'
+        img: 'imgs/egg_muffin.jpg',
+        text:'Our Egg McMuffin sandwich is an excellent source of protein. We place a freshly cracked Grade A egg on a toasted English Muffin topped with real butter and add lean Canadian bacon and melty American cheese. '
+
     },
-    {
-        id:4,
-        category:'breakfast',
-        description: 'Banana oatmeal',
-        price:12,
-        img: 'imgs/banana_oatmeal.jpg'
-    },
-    {
-        id:5,
-        category:'breakfast',
-        description: 'Banana strawberry smoothie',
-        price:10,
-        img: 'imgs/banana_straberry_smoothie.jpg'
-    },
-    {
-        id:6,
-        category:'lunch',
-        description: 'Beef burger with fries',
-        price:23,
-        img: 'imgs/beef_burger.jpg'
-    },*/
     {
         id:7,
         category:'lunch',
@@ -51,41 +32,6 @@ const menu = [
         img: 'imgs/beef_sandwitch.jpg',
         text:'Beef up your dinnertime with Italian Beef Sandwiches. Topped with drained giardiniera and shredded cheese, these Italian Beef Sandwiches are delicious and ready in under a half-an-hour.'
     },
-    /*{
-        id:8,
-        category:'drinks',
-        description: 'Beet root juice',
-        price:10,
-        img: 'imgs/beet_root_juice.jpg'
-    },
-    {
-        id:9,
-        category:'lunch',
-        description: 'Chicken with brocolli',
-        price:22,
-        img: 'imgs/chicken_brocolli.jpg'
-    },
-    {
-        id:10,
-        category:'breakfast',
-        description: 'Chocolate cake',
-        price:12,
-        img: 'imgs/chocolate_cake.jpg'
-    },
-    {
-        id:11,
-        category:'drinks',
-        description: 'Chocolate Shake',
-        price:17,
-        img: 'imgs/chocolate_shake.jpg'
-    },
-    {
-        id:12,
-        category:'drinks',
-        description: 'Coconut smoothie',
-        price:15,
-        img: 'imgs/coconut_smoothie.jpg'
-    },*/
     {
         id:13,
         category:'lunch',
@@ -94,190 +40,100 @@ const menu = [
         img: 'imgs/cod_fish_lunch.jpg',
         text:'Baked Cajun Garlic Butter Cod is a tender and juicy cod filet baked in a buttery cajun seasoning with garlic cloves and olive oil making this cod a flavorful and delicious lunch'
     },
-    /*{
-        id:14,
-        category:'breakfast',
-        description: 'Dark chocolate fantasy',
-        price:15,
-        img: 'imgs/dark_chocolate_cake.jpg'
-    },
-    {
-        id:15,
-        category:'breakfast',
-        description: 'Donuts',
-        price:12,
-        img: 'imgs/donuts.jpg'
-    },*/
     {
         id:16,
         category:'lunch',
         description: 'Egg with rice',
         price:18,
-        img: 'imgs/egg_and_rice.jpg'
+        img: 'imgs/egg_and_rice.jpg',
+        text:'Brown rice—higher in fiber and other nutrients than its white counterpart—is the perfect vehicle for this quick, protein-heavy lunch.'
     },
     {
         id:17,
         category:'lunch',
         description: 'Egg salad',
         price:18,
-        img: 'imgs/egg_salad.jpg'
+        img: 'imgs/egg_salad.jpg',
+        text:'Egg salad mixed with seasonings in the form of herbs, spices and other ingredients, bound with mayonnaise.'
     },
     {
         id:18,
         category:'breakfast',
         description: 'Fresh fruits',
         price:22,
-        img: 'imgs/fruits.jpg'
+        img: 'imgs/fruits.jpg',
+        text:'Fresh fruit, harvest crunch*, chia and coconut served with a kale-mango smoothie made with plain yogurt.'
     },
      {
         id:19,
         category:'drinks',
         description: 'Fruity smoothie',
         price:16,
-        img: 'imgs/fruity_smoothie.jpg'
+        img: 'imgs/fruity_smoothie.jpg',
+        text:'This is a great smoothie consisting of fruit, fruit juice and ice. Any kind of berry, mangos, papayas, kiwi fruit, et cetera make a great smoothie. Experiment with your favorites!'
     },
     {
         id:20,
         category:'lunch',
         description: 'Currey with rice',
         price:22,
-        img: 'imgs/indian_currey.jpg'
-    }, /*{
-        id:21,
-        category:'drinks',
-        description: 'Kale smoothie',
-        price:14,
-        img: 'imgs/kale-smoothie.jpg'
+        img: 'imgs/indian_currey.jpg',
+        text:'Tender pieces of chicken, carrots, and potatoes cooked in a rich savory curry sauce, this Indian version of curry is a must-have for your family meal. '
     }, {
-        id:22,
-        category:'drinks',
-        description: 'Kiwi punch',
-        price:12,
-        img: 'imgs/kiwi_punch.jpg'
-    }, */{
         id:23,
         category:'drinks',
-        description: 'latte',
+        description: 'Latte',
         price:8,
-        img: 'imgs/latte.jpg'
-    }, /*{
-        id:24,
-        category:'driks',
-        description: 'Lemon mint refresher',
-        price:12,
-        img: 'imgs/lemon_mint_refresher.jpg'
-    },*/ {
+        img: 'imgs/latte.jpg',
+        text:'Extra-smooth Espresso and milk—flavoured with hazelnut syrup and then poured over ice—creates an ideal cup for those who love a lighter-roasted coffee.'
+    }, {
         id:25,
         category:'breakfst',
         description: 'Oatmeal with greek yogurt',
         price:12,
-        img: 'imgs/oatmeal_greek_yogurt.jpg'
+        img: 'imgs/oatmeal_greek_yogurt.jpg',
+        text:'A blend of rolled and steel-cut oats with dried fruit, a nut medley and brown sugar as optional toppings. Hearty. Traditional. Classic.- VEGETARIAN'
     },
-    /*{
-        id:26,
-        category:'drinks',
-        description: 'Orange apricot juice',
-        price:16,
-        img: 'imgs/orange_apricot_juice.jpg'
-    },
-    {
-        id:27,
-        category:'drinks',
-        description: 'Orange smoothie',
-        price:16,
-        img: 'imgs/orange_smoothie.jpg'
-    },
-    {
-        id:28,
-        category:'breakfast',
-        description: 'Pancakes',
-        price:16,
-        img: 'imgs/pancakes.jpg'
-    },
-    {
-        id:29,
-        category:'lunch',
-        description: 'Paneer tikka rice',
-        price:24,
-        img: 'imgs/paneer_tikka_rice.jpg'
-    }*/,
     {
         id:30,
         category:'lunch',
-        description: 'Pork belly',
+        description: 'Grilled pork belly',
         price:26,
-        img: 'imgs/pork_belly.jpg'
+        img: 'imgs/pork_belly.jpg',
+        text:'Tender, gelatinous and aromatic, Chinese red braised pork belly is scrumptious and comforting.'
     },
     {
         id:31,
         category:'lunch',
         description: 'Potato fries',
         price:9,
-        img: 'imgs/potato_fries.jpg'
+        img: 'imgs/potato_fries.jpg',
+        text:'Fries made from locally sourced potatoes, cooked in sunflower oil blend and ready to be enjoyed with any of our internationally inspired dipping sauces. '
     },
-    /*{
-        id:32,
-        category:'lunch',
-        description: 'Salad',
-        price:16,
-        img: 'imgs/Salad.jpg'
-    }*/,
     {
         id:33,
         category:'lunch',
         description: 'Roasted pepper Soup',
         price:10,
-        img: 'imgs/soup.jpg'
+        img: 'imgs/soup.jpg',
+        text:'A rich flavoured soup made with sweet, red peppers, garlic, sun dried tomatoes topped with  homemade basil pesto and a creme fraiche swirl.'
     },
     {
         id:34,
         category:'lunch',
-        description: 'Speghetti',
+        description: 'Spaghetti',
         price:18,
-        img: 'imgs/spegattie.jpg'
+        img: 'imgs/spegattie.jpg',
+        text:'Spaghetti, hands down. Served with a simple salad of spring vegetables and finish with a creamy chocolate treat for a kid-friendly lunch menu that has plenty of grown-up appeal.'
     },
     {
         id:35,
         category:'lunch',
         description: 'Spinach sandwitch',
         price:16,
-        img: 'imgs/spinach_sandwitch.jpg'
+        img: 'imgs/spinach_sandwitch.jpg',
+        text:'Veggie sandwiches made with spinach and corn cooked in a creamy sauce. They also make a great lunch!'
     },
-    /*{
-        id:36,
-        category:'lunch',
-        description: 'Strawberry pizza',
-        price:18,
-        img: 'imgs/strawberry_pizza.jpg'
-    }
-    {
-        id:37,
-        category:'lunch',
-        description: 'Sushie',
-        price:22,
-        img: 'imgs/sushie.jpg'
-    },
-    {
-        id:38,
-        category:'lunch',
-        description: 'Veg. pizza',
-        price:18,
-        img: 'imgs/veg_pizza.jpg'
-    },
-    {
-        id:39,
-        category:'lunch',
-        description: 'Pasta in white sauce',
-        price:22,
-        img: 'imgs/white_pasta.jpg'
-    },
-    {
-        id:40,
-        category:'lunch',
-        description: 'Yam fries',
-        price:9,
-        img: 'imgs/yam_fries.jpg'
-    },*/
 ];
 
 const menuItems = document.querySelector('.menu-section');
@@ -290,6 +146,7 @@ window.addEventListener('DOMContentLoaded',function(){
 console.log(menuOptions);
 Array.from(menuOptions).forEach(function(menuBarItem){
     menuBarItem.addEventListener('click', function(event){
+        menuBarItem.classList.add('.active');
         console.log(event.target.innerText);
         console.log('dataid=',event.currentTarget.dataset.id);
         let seletedMenu = menu.filter(function(item){
