837041f94428cd262167571aa124f6710b86e378
punesing01 <puneeshsingh001@gmail.com>
2022-05-08 01:29:03 -0700

    added cart updates and deletion logic

--- app.js
+++ app.js
@@ -84,12 +84,12 @@ const menu = [
         id:23,
         category:'drinks',
         description: 'Latte',
-        price:8,
+        price:12,
         img: 'imgs/latte.jpg',
         text:'Extra-smooth Espresso and milk—flavoured with hazelnut syrup and then poured over ice—creates an ideal cup for those who love a lighter-roasted coffee.'
     }, {
         id:25,
-        category:'breakfst',
+        category:'breakfast',
         description: 'Oatmeal yogurt',
         price:12,
         img: 'imgs/oatmeal_greek_yogurt.jpg',
@@ -107,7 +107,7 @@ const menu = [
         id:31,
         category:'lunch',
         description: 'Potato fries',
-        price:9,
+        price:10,
         img: 'imgs/potato_fries.jpg',
         text:'Fries made from locally sourced potatoes, cooked in sunflower oil blend and ready to be enjoyed with any of our internationally inspired dipping sauces. '
     },
@@ -146,17 +146,36 @@ const navBar = document.querySelector('aside:nth-of-type(1)');
 const orderToCartClose =document.querySelector('.order-to-cart__content span');
 const orderToCartContainer = document.querySelector('.order-to-cart__container');
 
-const orderModalCloseButton = document.querySelector('.order-modal-close');
 const orderModal = document.querySelector('.order-modal-container');
-const plus = document.querySelector('.counter-container__div div:nth-of-type(3)');
-const minus = document.querySelector('.counter-container__div div:nth-of-type(1)');
-const count = document.querySelector('.counter-container__div div:nth-of-type(2)');
-const addToCartButton = document.querySelector('.order-to-cart__button');
-const displayRoot = document.querySelector('.order-modal-content');
+const plus=document.querySelector('.counter-container__div div:nth-of-type(3)');
+const minus=document.querySelector('.counter-container__div div:nth-of-type(1)');
+const count=document.querySelector('.counter-container__div div:nth-of-type(2)');
+const addToCartButton=document.querySelector('.order-to-cart__button');
+const displayRoot=document.querySelector('.order-modal-content');
+const toastMessage=document.querySelector('.toast_message');
+
+const cart=document.querySelector('.cart');
+const cartContainer=document.querySelector('.cart-container');
+
+const orderNow = document.querySelector('.modal-bar-list li:nth-of-type(5) a');
+const burgerMenu = document.querySelector('#burger-menu');
+const orderSummaryModalContainer= document.querySelector('.order-summary-modal__container');
+const orderSummaryModalClose=document.querySelector('.order-summary-modal__content > span');
+const orderSummaryDetails = document.querySelector('.order-summary-details');
+const orderSummaryDetailsHeader=document.querySelector('.order-summary-modal__content h4')
+ 
+const mainHeader = document.querySelector('h1');
 
 let counter = parseInt(count.innerText);
 
 let sideModal;
+let itemsAddedToCart = [];
+let itemNumber = 0;
+let orderSummayrHeader;
+let editItems;
+let deleteItems;
+let orderModalCloseButton;
+let renderedSideCart;
 
 window.addEventListener('DOMContentLoaded',function(){
      displayMenu(menu);
@@ -189,6 +208,7 @@ function displayMenu(menuToDisplay){
                     <div class="item-text">
                       <p>${item.description}</p>
                       <p>$${item.price}</p>
+                      <div class="item-order-quantity"></div>
                     </div>
                     <div class="item-desc">
                         <p>${item.text}</p>
@@ -197,14 +217,9 @@ function displayMenu(menuToDisplay){
                </artcile>
         </div>`;
     });
-   
     menuItems.innerHTML = newMenu.join('');
     return newMenu.join('');
 }
-
-const burgerMenu = document.querySelector('#burger-menu');
-const orderNow = document.querySelector('.modal-bar-list li:nth-of-type(5) a');
-const cart = document.querySelector('.burger-menu-bar-list li:last-of-type div');
 
 sideModal = document.createElement('div');
 const modalList = `
@@ -235,15 +250,15 @@ navSideModal.append(sideModal);
 
 const sideModalCloseButton = document.querySelector('.side-modal-close-button');
 const sideModalContainer = document.querySelector('.side-modal-container');
- 
+const closeCart = document.querySelector('.cart > div');
+
 sideModalCloseButton.addEventListener('click',function(){
     sideModalContainer.style.display='none';
 });
 
 burgerMenu.addEventListener('click',function(){
-    console.log('hello');
     sideModalContainer.style.display='flex';
-    const sideModalOrderNow = document.querySelector('#side-modal .side-bar-list li:last-of-type a'); 
+    const sideModalOrderNow = document.querySelector('#side-modal .side-bar-list  li:nth-of-type(4) a'); 
     const menuBar = document.querySelector('.menu-bar');
 
     sideModalOrderNow.addEventListener('click',function(){
@@ -270,12 +285,15 @@ window.addEventListener('click',function(event){
 orderNow.addEventListener('click',function(event){
     console.log('Order now');
     orderModal.style.display='block';
-    displayMenuInModal();
- });
+    //mainHeader.style.display='none';
 
-orderModalCloseButton.addEventListener('click',function(){
-    orderModal.style.display='none';
-});
+    displayMenuInModal();
+    const orderModalBackButton=document.querySelector('.order-modal-header__back');
+    orderModalBackButton.addEventListener('click',function(){
+        //displayRoot.style.display='none';
+        orderModal.style.display='none';
+    });
+ });
 
 window.addEventListener('click',function(event){
      if(event.target === sideModalContainer){
@@ -289,17 +307,29 @@ window.addEventListener('click',function(event){
     if(event.target === displayRoot | event.target === orderModal){
         orderToCartContainer.style.display='none';
     }
+
+    if(event.target === orderSummaryModalContainer){
+        orderSummaryModalContainer.style.display='none';
+    }
 });
 
 function displayMenuInModal(){
     const menuToDisplay = displayMenu(menu);
-    displayRoot.innerHTML = menuToDisplay;
-
+    const modalHeader = 
+                        `<header class="order-modal-header">  
+                            <div>
+                                <span class="order-modal-header__back">&#x21e6;</span>
+                                <span class="order-modal-close">&times;</span>
+                                <h4 style="text-align:center;font-size:x-large;color:maroon;">Select items for your order</h4>
+                            </div>
+                        </header>`;
+    displayRoot.innerHTML = modalHeader.concat(menuToDisplay);
+    orderModalCloseButton = document.querySelector('.order-modal-close');
     const listOfFood = document.querySelectorAll('.order-modal-content .item-container');
   
     console.log('displayRoot=',listOfFood);
 
-    Array.from(listOfFood).forEach(function(food){
+    Array.from(listOfFood).forEach(function(food,index,event){
         food.addEventListener('click',function(event){
             const itemName = food.querySelector('.item-detail-container .item-text p:nth-of-type(1)').textContent;
             const itemPrice=food.querySelector('.item-detail-container .item-text p:nth-of-type(2)').textContent;
@@ -313,7 +343,13 @@ function displayMenuInModal(){
             counter=1;
             console.log('counter=',counter);
             addToCartButton.textContent = `Add - ${itemPrice}`;
+            itemNumber=index; 
         });
+    });
+
+    orderModalCloseButton.addEventListener('click',function(){
+        console.log('Close order modal');
+        orderModal.style.display='none';
     });
 }
 
@@ -321,12 +357,7 @@ orderToCartClose.addEventListener('click',function(){
     orderToCartContainer.style.display='none'; 
     counter=1;
     count.innerText=counter;
-});
-
-
-function addToCartModal(){
-
-}
+ });
 
 plus.addEventListener('click',function(){
   updateCount('+');
@@ -356,5 +387,187 @@ function updatePrice(){
          return itemDesc.textContent === item.description? item.price : null; 
      });
      console.log(itemPrice[0].price);
-     addToCartButton.textContent = `Add -$${(parseInt(itemPrice[0].price) * counter).toString()}`;
- }
+     addToCartButton.textContent = `Add - $${(parseInt(itemPrice[0].price) * counter).toString()}`;
+}
+
+ addToCartButton.addEventListener('click',function(e){
+    const itemChosen = document.querySelector('.order-to-cart__content h4');
+    const itemQuantity = document.querySelector('.counter-container__div .count').textContent;
+    const itemPrice =document.querySelector('.order-to-cart__button').textContent.substring(7);
+    console.log('itemPrice=',itemPrice);
+    console.log('itemChosen=',itemChosen.textContent);
+    const itemKey = itemChosen.textContent;
+    const addItem = {};
+    addItem.itemName = itemChosen.textContent;
+    addItem.itemPrice=itemPrice;
+    addItem.quantity=itemQuantity;
+    addItem.itemPricePerQuantity=itemQuantity > 1? parseInt(parseInt(itemPrice)/parseInt(itemQuantity)):itemPrice;
+    itemsAddedToCart.push(addItem);
+    showToaster('Item has been added to cart.','flex');
+    orderToCartContainer.style.display='none';
+    console.log(itemsAddedToCart);
+
+    const orderedQuantity = document.querySelectorAll('.order-modal-content .item-order-quantity');
+    orderedQuantity[itemNumber].innerText = itemQuantity;
+    orderedQuantity[itemNumber].style.backgroundColor='maroon';
+    orderedQuantity[itemNumber].style.color='white';
+    const itemContainers = document.querySelectorAll('.order-modal-content .item-container');
+    itemContainers[itemNumber].style.border='2px solid maroon';
+    itemContainers[itemNumber].style.boxShadow='none';
+    
+    if(!document.querySelector('.side-bar-list .cart')){
+        const sideBarListOrderNow = document.querySelector('.side-bar-list li:nth-of-type(4)');
+        console.log('inserting side cart');
+        const sideCart = `<li><div class="cart">
+                            <div>x</div>
+                            <div class="cart-container">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M96 0C107.5 0 117.4 8.19 119.6 19.51L121.1 32H541.8C562.1 32 578.3 52.25 572.6 72.66L518.6 264.7C514.7 278.5 502.1 288 487.8 288H170.7L179.9 336H488C501.3 336 512 346.7 512 360C512 373.3 501.3 384 488 384H159.1C148.5 384 138.6 375.8 136.4 364.5L76.14 48H24C10.75 48 0 37.25 0 24C0 10.75 10.75 0 24 0H96zM128 464C128 437.5 149.5 416 176 416C202.5 416 224 437.5 224 464C224 490.5 202.5 512 176 512C149.5 512 128 490.5 128 464zM512 464C512 490.5 490.5 512 464 512C437.5 512 416 490.5 416 464C416 437.5 437.5 416 464 416C490.5 416 512 437.5 512 464z"></path></svg>
+                            </div>
+                        </div></li>`;
+        sideBarListOrderNow.insertAdjacentHTML('afterend',sideCart);
+        renderedSideCart = document.querySelector('.side-bar-list .cart');
+        renderedSideCart.style.display='flex';
+
+        renderedSideCart.addEventListener('click',function(){
+            console.log('Cart has been selected');
+
+            const sideModalContainer = document.querySelector('.side-modal-container');
+                 sideModalContainer.style.display='none';
+                 displayOrderDetails();
+             }
+         );
+
+         const sideCartClose = document.querySelector('.side-bar-list .cart div:first-of-type');
+         sideCartClose.addEventListener('click',function(e){
+            let closeFlag = window.confirm('Do you wish to delete your cart?');
+            if(closeFlag){
+                renderedSideCart.style.display='none';
+            }
+         });
+    } else  renderedSideCart.style.display='flex';
+ });
+
+function toggleCartAndOrderNowButton(counterStatus){
+    cart.style.display= counterStatus;
+    if(cart.style.display === 'flex'){
+        console.log('display cart');
+        console.log('hide order now');
+        //orderNow.style.display='none';
+    } else if(cart.style.display='none'){
+        orderNow.style.display='flex';
+        console.log('display order now');
+     }else orderNow.style.display=counterStatus;
+}
+
+(function createCartHeader(){
+    orderSummayrHeader = document.createElement('div');
+    orderSummayrHeader.setAttribute('class','order-summary-row');
+    orderSummayrHeader.setAttribute('style','gap: 0.7rem;');
+    
+    const orderItemName= document.createElement('div');  
+    orderItemName.textContent='Item Name';
+    orderItemName.setAttribute('style','flex-basis: 20%;');
+
+    const orderItemQuatity= document.createElement('div');  
+    orderItemQuatity.textContent='Quantity';
+    const orderItemPrice= document.createElement('div');  
+    orderItemPrice.textContent='Item price';
+    const orderItemTotalPrice= document.createElement('div');  
+    orderItemTotalPrice.textContent='Total Price';
+
+    orderSummayrHeader.append(orderItemName);
+    orderSummayrHeader.append(orderItemQuatity);
+    orderSummayrHeader.append(orderItemPrice);
+    orderSummayrHeader.append(orderItemTotalPrice);
+})();
+
+cartContainer.addEventListener('click',function(e){
+   console.log('Cart has been selected');
+        displayOrderDetails();
+    }
+);
+
+closeCart.addEventListener('click',function(){
+    console.log('Close cart');
+    let closeFlag = window.confirm('Do you wish to delete your cart?');
+    if(closeFlag){
+        showToaster('Your cart has been deleted','none');
+        const orderSummary = document.querySelector('.order-modal-container > div');
+        orderSummary.innerHTML = '';
+        //orderModal.style.display='none';
+        orderSummaryDetails.innerHTML='';
+        itemsAddedToCart=[];
+    }
+});
+
+function showToaster(message,showCart){
+    toastMessage.textContent = message;
+    toastMessage.style.display='block';
+     setTimeout(()=>{
+        toastMessage.style.display='none';
+        toggleCartAndOrderNowButton(showCart);
+    },2000);
+}
+
+orderSummaryModalClose.addEventListener('click',function(){
+    console.log('Close order summary');
+    orderSummaryModalContainer.style.display='none';
+});
+
+function displayOrderDetails(){
+    console.log('itemsAddedToCart=',itemsAddedToCart);
+    orderSummaryDetailsHeader.insertAdjacentElement('afterend',orderSummayrHeader);
+         const orderToDisplay = itemsAddedToCart.map(function(item){
+            return  `<div>
+                         <div class="order-summary-row">
+                            <div>${item.itemName}</div>
+                            <div>${item.quantity}</div>
+                            <div>$${item.itemPricePerQuantity}</div>
+                            <div>$${item.itemPrice}</div>
+                            <div class="edit">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M421.7 220.3L188.5 453.4L154.6 419.5L158.1 416H112C103.2 416 96 408.8 96 400V353.9L92.51 357.4C87.78 362.2 84.31 368 82.42 374.4L59.44 452.6L137.6 429.6C143.1 427.7 149.8 424.2 154.6 419.5L188.5 453.4C178.1 463.8 165.2 471.5 151.1 475.6L30.77 511C22.35 513.5 13.24 511.2 7.03 504.1C.8198 498.8-1.502 489.7 .976 481.2L36.37 360.9C40.53 346.8 48.16 333.9 58.57 323.5L291.7 90.34L421.7 220.3zM492.7 58.75C517.7 83.74 517.7 124.3 492.7 149.3L444.3 197.7L314.3 67.72L362.7 19.32C387.7-5.678 428.3-5.678 453.3 19.32L492.7 58.75z"/></svg>
+                            </div>
+                            <div class="delete">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM31.1 128H416V448C416 483.3 387.3 512 352 512H95.1C60.65 512 31.1 483.3 31.1 448V128zM111.1 208V432C111.1 440.8 119.2 448 127.1 448C136.8 448 143.1 440.8 143.1 432V208C143.1 199.2 136.8 192 127.1 192C119.2 192 111.1 199.2 111.1 208zM207.1 208V432C207.1 440.8 215.2 448 223.1 448C232.8 448 240 440.8 240 432V208C240 199.2 232.8 192 223.1 192C215.2 192 207.1 199.2 207.1 208zM304 208V432C304 440.8 311.2 448 320 448C328.8 448 336 440.8 336 432V208C336 199.2 328.8 192 320 192C311.2 192 304 199.2 304 208z"/></svg>
+                            </div>
+                         </div>
+                    </div>`;
+        });
+ 
+    if(itemsAddedToCart.length){
+        orderSummaryDetails.setAttribute('style','color:black;');
+        orderSummaryDetails.innerHTML=orderToDisplay.join('');
+    } else {
+        orderSummaryDetails.innerHTML=`<div>No more items are found in the cart...</div>`
+        orderSummaryDetails.setAttribute('style','text-align:center;font-weight:bolder;color:red;');
+        clearCart();
+    }
+    orderSummaryModalContainer.style.display='block';
+    editItems = document.querySelectorAll('.edit svg');
+    deleteItems = document.querySelectorAll('.delete svg');
+
+    editItems.forEach(function(edit,index){
+        console.log('editing....');
+        edit.addEventListener('click',function(){
+            console.log(`${index} item is clicked`);
+        });
+    });
+
+    deleteItems.forEach(function(deleteItem,index){
+        console.log('deleting....');
+        deleteItem.addEventListener('click',function(){
+            console.log(`${index} item is clicked`);
+            console.log(itemsAddedToCart[index]);
+            itemsAddedToCart.splice(index,1);
+            //re-render again after every delete operation
+            displayOrderDetails();
+        });
+    });
+}
+
+function clearCart(){
+    cart.style.display='none'; 
+    renderedSideCart.style.display='none';
+    counter=1;
+    count.innerText=counter;
+}
